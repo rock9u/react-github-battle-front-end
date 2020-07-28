feec447bb7169c14cd4dd724c3b2c1e2c778b0e9
Rock Gu <guf@mcmaster.ca>
2020-07-28 15:54:23 -0400

    GB - :fire: stateful navbar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+    });
+  }
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
     return (
-      <ul className='flex-center'>
-        {languages.map(language => {
-          return <li key={language}>
-            <button className='btn-clear nav-link'>
-              {language}
-            </button>
-          </li>
+      <ul className="flex-center">
+        {languages.map((language) => {
+          return (
+            <li key={language}>
+              <button
+                className="btn-clear nav-link"
+                style={
+                  language === this.state.selectedLanguage
+                    ? { color: "palevioletred" }
+                    : null
+                }
+                onClick={() => this.updateLanguage(language)}
+              >
+                {language}
+              </button>
+            </li>
+          );
         })}
       </ul>
-    )
+    );
   }
 }
