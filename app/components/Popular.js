eef58bf6c04a333b7b4ba48e2ec38b56886ab711
Rock Gu <guf@mcmaster.ca>
2020-07-28 21:50:31 -0400

    GB - :zap: format card list

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 
 function LanguageNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -26,6 +27,53 @@ function LanguageNav ({ selected, onUpdateLanguage }) {
 LanguageNav.propTypes = {
   selected: propTypes.string.isRequired,
   onUpdateLanguage: propTypes.func.isRequired
+}
+
+function ReposGrid ({ repos }) {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+        const { login, avatar_url } = owner
+
+        return (
+          <li key={html_url} className='repo bg-light'>
+            <h4 className='header-lg center-text'>
+              #{index + 1}
+            </h4>
+            <img
+              className='avatar'
+              src={avatar_url}
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className='center-text'>
+              <a className='link' href={html_url}>{login}</a>
+            </h2>
+            <ul className='card-list'>
+              <li>
+                <FaUser color='orange' size={22} />
+                <a href={`https://github.com/${login}`}>
+                  {login}
+                </a>
+              </li>
+              <li>
+                <FaStar color='gold' size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color='LightSkyBlue' size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color='indianRed' size={22} />
+                {open_issues.toLocaleString()} open issues
+              </li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default class Popular extends React.Component {
@@ -88,7 +136,7 @@ export default class Popular extends React.Component {
         />
         {error && <p>Error⛔️</p>}
         {this.isLoading() && <p>Loading😃</p>}
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </>
     )
   }
